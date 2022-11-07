65861095fba2de1187d8909c23bc542a24bb5b2c
shanmugam ss <shanmugam.subramanian@gmail.com>
2022-11-07 12:05:45 +0530

    V03 is being committed.

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo 02',
+      title: 'Flutter Demo 03',
       theme: ThemeData(
         // This is the theme of your application.
         //
