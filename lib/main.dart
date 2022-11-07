ed01ecb339298d3732d258b66a2791dd535f36e5
shanmugam ss <shanmugam.subramanian@gmail.com>
2022-11-07 12:20:05 +0530

    V05 is being committed.

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo 04',
+      title: 'Flutter Demo 05',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page 03'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page 05'),
     );
   }
 }
